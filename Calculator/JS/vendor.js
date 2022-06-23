957d69641ba48c0bcda2a58574a5c0116847ec01
Patel-Mohammed-Ali <74188800+patel9619ali@users.noreply.github.com>
2022-06-23 19:44:24 +0530

    Add files via upload

--- Calculator/JS/vendor.js
+++ Calculator/JS/vendor.js
@@ -2,28 +2,41 @@ class Calculator{
     constructor(previousOperand , currentResult){
         this.previousOperand = previousOperand;
         this.currentResult = currentResult;
-        this.clear();
     }
-    clear(){
-        currentResult.setAttribute('value', '');
+    clear(currentResult){
+        clearAll.addEventListener('click' , function(){
+               currentResult.innerHTML.length > 1 ? currentResult.innerHTML = '' : currentResult.innerHTML = undefined;
+        })
+        
     }    
+    delete(previousOperand){
+        deleteString.addEventListener("click" , function(){
+            console.log("abc");
+            currentResult
+        })
+    }
+    appendNumber(){
+
+    }
+
 }
 
+
+let buttonAll = document.querySelectorAll("[type='button']")
 let selectNumber = document.querySelectorAll("[name='number-operand']");
 let signOperand = document.querySelectorAll("[name='sign-operand']");
 let previousOperand = document.getElementById("resultBefore");
 let currentResult = document.getElementById("result");
 let clearAll = document.getElementById("clearAll");
-
-
+let deleteString = document.getElementById("delete_backString");
 const calculator = new Calculator(previousOperand , currentResult);
-console.log(clearAll);
 
-selectNumber.forEach(function(userItem) {
-    userItem.addEventListener("click" , function(){
-        let valueOfUserItem = userItem.value;
-        calculator.clear();
-        console.log(valueOfUserItem);
-        currentResult.setAttribute('value', valueOfUserItem);
+buttonAll.forEach(function (buttonList) {
+    buttonList.addEventListener("click" ,function () {
+        currentResult.innerHTML = buttonList.value; 
+        console.log(currentResult.innerHTML.length); 
+        calculator.clear(currentResult);
+        calculator.delete(previousOperand);
     })
+    
 })
