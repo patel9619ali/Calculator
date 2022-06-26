86e4e8b07339a4c123548be43d109eae4a6c9d61
Patel-Mohammed-Ali <74188800+patel9619ali@users.noreply.github.com>
2022-06-27 01:13:50 +0530

    Add files via upload

--- Calculator/JS/vendor.js
+++ Calculator/JS/vendor.js
@@ -5,24 +5,43 @@ class Calculator{
     }
     clear(currentResult){
         clearAll.addEventListener('click' , function(){
-               currentResult.innerHTML.length > 1 ? currentResult.innerHTML = '' : currentResult.innerHTML = undefined;
+            currentResult.innerHTML.length > 0 ? currentResult.innerHTML = '' :  currentResult.innerHTML = '';
         })
         
     }    
-    delete(previousOperand){
+    delete(currentResult){
         deleteString.addEventListener("click" , function(){
-            console.log("abc");
-            currentResult
+           let firstNumber = currentResult.innerHTML;
+        //    firstNumber = firstNumber.substr(0 , firstNumber.length-1);
+        //    console.log(firstNumber);
+           for (let i = firstNumber; i < firstNumber.length; i--) {
+            // if (firstNumber[i] > 0) {
+                firstNumber = firstNumber.substr(0 , firstNumber.length-1);
+                currentResult.innerHTML =  firstNumber;
+            // }
+          }
+        //    currentResult.innerHTML = firstNumber;
+        //    if (currentResult.innerHTML.length > 0) {
+            // let secondNumber = currentResult.innerHTML.slice(0 , currentResult.innerHTML.length -1);
+            // currentResult.innerHTML =  secondNumber; 
+            // console.log(currentResult.innerHTML);
+            // console.log("true");
+        //    }
+        //    else{
+        //     console.log("False");
+        //     }
+
+        //    currentResult.innerHTML = secondNumber;
         })
     }
-    appendNumber(){
-
+    appendNumber(number){
+        currentResult.innerHTML = currentResult.innerHTML + number.toString();
     }
 
 }
 
 
-let buttonAll = document.querySelectorAll("[type='button']")
+let buttonAll = document.querySelectorAll(".numberList");
 let selectNumber = document.querySelectorAll("[name='number-operand']");
 let signOperand = document.querySelectorAll("[name='sign-operand']");
 let previousOperand = document.getElementById("resultBefore");
@@ -30,13 +49,10 @@ let currentResult = document.getElementById("result");
 let clearAll = document.getElementById("clearAll");
 let deleteString = document.getElementById("delete_backString");
 const calculator = new Calculator(previousOperand , currentResult);
-
 buttonAll.forEach(function (buttonList) {
     buttonList.addEventListener("click" ,function () {
-        currentResult.innerHTML = buttonList.value; 
-        console.log(currentResult.innerHTML.length); 
+        calculator.appendNumber(buttonList.value);
         calculator.clear(currentResult);
-        calculator.delete(previousOperand);
-    })
-    
+        calculator.delete(currentResult);
+    })  
 })
